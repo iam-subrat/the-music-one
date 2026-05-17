17df8b0c85378dbdc5702cd8baade4ebf500e4e9
Justin Truman <37294184+su-brat@users.noreply.github.com>
2026-05-17 22:54:14 +0530

    feat: add base component barrel export

---
+++ ui/src/components/base/index.js
@@ -0,0 +1,6 @@
+// ui/src/components/base/index.js
+export { default as NeuSurface } from './NeuSurface';
+export { default as NeuButton } from './NeuButton';
+export { default as NeuInput } from './NeuInput';
+export { default as NeuIconWrapper } from './NeuIconWrapper';
+export { default as PlatformIcon } from './PlatformIcon';
