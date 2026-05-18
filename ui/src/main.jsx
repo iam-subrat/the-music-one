a8b31ed64e09217cc63e5fff103c9c01a78f96a9
Justin Truman <37294184+su-brat@users.noreply.github.com>
2026-05-18 11:11:01 +0530

    toast on copy

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { ToastProvider } from './components/Toast';
 import { loadFlags } from './lib/flags';
 import './styles/base.css';
 
@@ -9,7 +10,9 @@ loadFlags().finally(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </BrowserRouter>
     </React.StrictMode>
   );
